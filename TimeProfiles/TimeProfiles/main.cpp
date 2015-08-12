3b529c1aea12f4dd9e56d77b507f753fb92d4cae
Timothy Place <tim@cycling74.com>
2015-08-12 09:03:40 -0500

    adding test with dataspace conversion

--- TimeProfiles/TimeProfiles/main.cpp
+++ TimeProfiles/TimeProfiles/main.cpp
@@ -28,37 +28,64 @@ int main(int argc, const char * argv[])
 	
 	
 	// First Test: set an attribute 100,000 times
+	{
+		TTAudioObject	g1("gain");
+		Jamoma::Gain	g2;
+		MTRand			r;
 
-	TTAudioObject	g1("gain");
-	Jamoma::Gain	g2;
-	MTRand			r;
+		auto start1 = std::chrono::steady_clock::now();
+		
+		for (int i=0; i<1000000; i++)
+			g1.set("linearGain", r.rand(1.0));
+		
+		auto finish1 = std::chrono::steady_clock::now();
 
-	auto start1 = std::chrono::steady_clock::now();
-	
-	for (int i=0; i<1000000; i++)
-		g1.set("gain", r.rand(1.0));
-	
-	auto finish1 = std::chrono::steady_clock::now();
+		
+		auto start2 = std::chrono::steady_clock::now();
+		
+		for (int i=0; i<1000000; i++)
+			g2.gain = r.rand(1.0);
+		
+		auto finish2 = std::chrono::steady_clock::now();
 
+		std::cout << std::endl;
+		std::cout << "Set the (linear) `gain` Parameter of the `Gain` class 1 million times:"  << std::endl;
+		std::cout << "Jamoma1 = " <<  std::chrono::duration<double, std::milli>(finish1 - start1).count()
+		<< " ms       Jamoma2 = "  <<  std::chrono::duration<double, std::milli>(finish2 - start2).count() << " ms"  << std::endl;
+	}
 	
-	auto start2 = std::chrono::steady_clock::now();
 	
-	for (int i=0; i<1000000; i++)
-		g2.gain = r.rand(1.0);
-	
-	auto finish2 = std::chrono::steady_clock::now();
+	// Test: set an attribute 100,000 times with db conversion
+	{
+		TTAudioObject	g1("gain");
+		Jamoma::Gain	g2;
+		MTRand			r;
+		
+		auto start1 = std::chrono::steady_clock::now();
+		
+		for (int i=0; i<1000000; i++)
+			g1.set("gain", r.rand(100.0) - 50.0);
+		
+		auto finish1 = std::chrono::steady_clock::now();
+		
+		
+		auto start2 = std::chrono::steady_clock::now();
+		
+		for (int i=0; i<1000000; i++)
+			g2.gain = std::make_pair(r.rand(100.0) - 50.0, Jamoma::Unit::Db); ;
+		
+		auto finish2 = std::chrono::steady_clock::now();
+		
+		std::cout << std::endl;
+		std::cout << "Set the db `gain` Parameter of the `Gain` class 1 million times:"  << std::endl;
+		std::cout << "Jamoma1 = " <<  std::chrono::duration<double, std::milli>(finish1 - start1).count()
+		<< " ms       Jamoma2 = "  <<  std::chrono::duration<double, std::milli>(finish2 - start2).count() << " ms"  << std::endl;
+		
+		// Note that with the db conversion Jamoma1 is actually a little faster than Jamoma1 with linear gain!
+		// This is because the conversion uses a custom setter which is faster than the default setter in Jamoma1.
+	}
 
-	std::cout << std::endl;
-	std::cout << "Set the `gain` Parameter of the `Gain` class 1 million times:"  << std::endl;
-	std::cout << "Jamoma1 = " <<  std::chrono::duration<double, std::milli>(finish1 - start1).count()
-	<< " ms       Jamoma2 = "  <<  std::chrono::duration<double, std::milli>(finish2 - start2).count() << " ms"  << std::endl;
-	std::cout << std::endl;
-	
-	
-	
-	
-	
-	
+
 	
 	
 	return 0;
